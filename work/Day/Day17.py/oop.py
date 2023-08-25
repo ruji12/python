6ee744e40ff63b7d84412f43db52626afeabded3
rujita lage <rushalilage12@gmail.com>
2023-08-25 21:27:32 +0545

    code

---
+++ work/Day/Day17.py/oop.py
@@ -0,0 +1,21 @@
+#pascal case for class name ClassExampleInPascalCase
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.follwers = 0
+        self.follwing = 0
+    def follow(self, user):
+        user.follwers +=1
+        self.follwing +=1
+    
+user_1=User(5, "Tilak kandel")
+user_2=User(4, "Santosh Kumar Poudel")
+
+print(user_1.username)
+
+user_1.follow(user_2)
+print(user_1.follwers)
+print(user_1.follwing)
+print(user_2.follwers)
+print(user_2.follwing)
